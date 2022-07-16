e53cbf87623a7b5c168505b4b270fe7623286a01
fede999 <partesanofederico@gmail.com>
2022-07-16 18:56:07 +0200

    feat(package-lock.json, package.json, completePush.ts): add node-emoji package

--- src/completePush.ts
+++ src/completePush.ts
@@ -2,12 +2,12 @@ import inquirer from "inquirer";
 import { git } from ".";
 import { doCommit } from "./commit";
 import { loader } from "./utils";
-
+import emoji from "node-emoji"
 
 export const completePush = async () => {
   await doCommit(true);
    const {updateBottomBar} = await loader();
-
+  const aries = emoji.get("aries")
    await git.push();
-   updateBottomBar("\x1b[32mSuccessfully pushed! \&#38;&#35;&#49;&#50;&#56;&#53;&#49;&#50;")
+   updateBottomBar(`\x1b[32mSuccessfully pushed! ${aries}`)
 };
