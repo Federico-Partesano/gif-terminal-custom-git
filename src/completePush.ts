cc03dae2a95800015dbc01044c57048edf3d8776
fede999 <partesanofederico@gmail.com>
2022-07-16 16:41:27 +0200

    fix(completePush.ts): d

--- src/completePush.ts
+++ src/completePush.ts
@@ -4,6 +4,6 @@ import { doCommit } from "./commit";
 export const completePush = async() => {
     await doCommit(true);
     await git.push();
-    console.log('\x1b[36m%s\x1b[0m', 'pushed');  //cyan
+    console.log("\x1b[32m", 'pushed');  //cyan
 
 }
