7eb7fcae57d5f544128ec93d9b10dcd89e988be6
fede999 <partesanofederico@gmail.com>
2022-07-16 18:47:55 +0200

    fix(utils.ts): loader

--- src/utils.ts
+++ src/utils.ts
@@ -2,7 +2,7 @@ import inquirer from "inquirer";
 
 export const getError = (error: any) => new Error(`${error}`);
 
-let chars = ['\/', '\/', '\/', '\/'];
+let chars = ['\/', "|", "/", "⎯"];
 
 export const loader = async () => {
   const ui = new inquirer.ui.BottomBar();
