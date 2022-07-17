c803a097287249c26ee248e771deebf6c69027c2
Federico-Partesano <partesanofederico@hotmail.it>
2022-07-17 11:06:13 +0200

    update completePush

--- src/completePush.ts
+++ src/completePush.ts
@@ -1,17 +1,37 @@
 import inquirer from "inquirer";
 import { git } from ".";
 import { doCommit } from "./commit";
-import { loader } from "./utils";
+import { isGitError, loader } from "./utils";
 import emoji from "node-emoji";
 import { exit } from "process";
 import chalk from "chalk";
+
+const pushNewBranch = async () => {
+  try {
+    const {current} = await git.branchLocal();
+    await git.push("origin", current, ["--set-upstream"]);
+  } catch (e) {
+    console.log("e", e);
+    exit(1)
+  }
+};
+
 
 export const completePush = async (exitOnFinish: boolean = false) => {
   await doCommit(true);
   const { updateBottomBar } = await loader();
   const grinning = emoji.get("grinning");
-  await git.push();
-  updateBottomBar(`${chalk.green("Successfully pushed!")} ${grinning}`);
+  try {
+    await git.push();
+    updateBottomBar(`${chalk.green("Successfully pushed!")} ${grinning}`);
+  } catch (e) {
+    if (isGitError(e)) {
+      if (e.message.includes("git push --set-upstream origin")) {
+        await pushNewBranch();
+        updateBottomBar(`${chalk.green("Successfully pushed!")} ${grinning}`);
+      }
+    }
+  }
   console.log(" ");
   exitOnFinish && exit(1);
 };
